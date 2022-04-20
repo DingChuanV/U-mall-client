cf1483dae1ac66c60efe714ebd3b66b04cd5e8d1
wanglufei <2686221966@qq.com>
2022-04-20 22:28:00 +0800

    完成网关请求到其他服务

--- static/config/index.js
+++ static/config/index.js
@@ -1,11 +1,13 @@
+/* eslint-disable no-multi-spaces */
+/* eslint-disable semi */
 /**
  * 开发环境
  */
 ;(function () {
   window.SITE_CONFIG = {};
 
-  // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080/renren-fast';
+  // api接口请求地址 定义为给网关发请求 88
+  window.SITE_CONFIG['baseUrl'] = 'http://localhost:88/api';
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名

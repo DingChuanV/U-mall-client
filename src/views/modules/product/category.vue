51ed59336687d308c6c636e26b84173285507276
wanglufei <2686221966@qq.com>
2022-04-21 09:25:35 +0800

    实现分类的显示

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -6,11 +6,19 @@
   版本：v1.0
 -->
 <template>
-  <el-tree
-    :data="data"
-    :props="defaultProps"
-    @node-click="handleNodeClick"
-  ></el-tree>
+  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick">
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+      <span>{{ node.label }}</span>
+      <span>
+        <el-button type="text" size="mini" @click="() => append(data)">
+          增加
+        </el-button>
+        <el-button type="text" size="mini" @click="() => remove(node, data)">
+          删除
+        </el-button>
+      </span>
+    </span>
+  </el-tree>
 </template>
 
 <script>
@@ -23,10 +31,11 @@ export default {
   props: {},
   data() {
     return {
-      data: [],
+      menus: [],
       defaultProps: {
+        //子菜单的数据
         children: "children",
-        label: "label",
+        label: "name",
       },
     };
   },
@@ -39,7 +48,9 @@ export default {
         url: this.$http.adornUrl("/product/category/list/tree"),
         method: "get",
       }).then(({ data }) => {
-        console.log * ("收到的分类数据", data);
+        //({data})将数据解构
+        console.log("收到的分类数据", data.data);
+        this.menus = data.data;
       });
     },
   },
